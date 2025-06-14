9ff29ef8f70f549f503869317b0a83f6f92d70cb
VincentVisage <144054905+VincentVisage@users.noreply.github.com>
2025-06-15 02:00:23 +0300

    SQLAlchemy 2.0 | alembic

--- core/config.py
+++ core/config.py
@@ -1,12 +1,22 @@
 from pydantic_settings import BaseSettings
+from pydantic import BaseModel
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
 
-class Setting(BaseSettings):
+DB_PATH = BASE_DIR / "db.sqlite3"
+
+
+class DBSettings(BaseModel):
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    echo: bool = False
+    # echo: bool = True
+
+
+class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
-    db_echo: bool = True
 
+    db: DBSettings = DBSettings()
+    
 
-settings = Setting()
+settings = Settings()
--- core/models/post.py
+++ core/models/post.py
@@ -9,11 +9,17 @@ class Post(UserRelationMixin, Base):
     _user_back_populates = 'posts'
     # _user_id_nullable = False
 
-
-
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(
         Text,
         default="",
         server_default="",
     )
+
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, title={self.title!r}), user_id={self.user_id}"
+    
+
+    def __repr__(self):
+        return str(self)
